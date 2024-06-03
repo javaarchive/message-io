224d040f13db7c99327155ab0c6fef8170692d0f
Raymond <20248577+javaarchive@users.noreply.github.com>
2024-06-03 15:33:01 -0700

    attempt to write unix socket send_to

--- src/adapters/unix_socket.rs
+++ src/adapters/unix_socket.rs
@@ -375,6 +375,32 @@ impl Local for DatagramLocalResource {
             };
         }
     }
+
+    fn send_to(&self, _addr: NetworkAddr, data: &[u8]) -> SendStatus {
+        let unix_addr: mio::net::SocketAddr = match _addr {
+            NetworkAddr::Unix(addr) => addr,
+            _ => panic!("Internal error: Got wrong addr type for unix datagram socket send_to"),
+        };
+        // from udp.rs
+        let path = unix_addr.try_get_pathname().expect("I can't send to this.");
+        loop {
+            match self.listener.send_to(data, path) {
+                Ok(_) => break SendStatus::Sent,
+                // Avoid ICMP generated error to be logged
+                Err(ref err) if err.kind() == ErrorKind::ConnectionRefused => {
+                    break SendStatus::ResourceNotFound
+                }
+                Err(ref err) if err.kind() == ErrorKind::WouldBlock => continue,
+                Err(ref err) if err.kind() == ErrorKind::Other => {
+                    break SendStatus::MaxPacketSizeExceeded
+                }
+                Err(err) => {
+                    log::error!("unix socket send error: {}", err);
+                    break SendStatus::ResourceNotFound // should not happen
+                }
+            }
+        }
+    }
 }
 
 impl Drop for DatagramLocalResource {
